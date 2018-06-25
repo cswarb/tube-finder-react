7918d7d21421f6634872275aacf49f8cd8b6d5fa
Chris Swarbrick <chris.swarb@gmail.com>
2018-06-25 22:21:30 +0100

    [development] - Small updates to tslint and methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import * as React from "react";
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div id="container">
-            <div className="navigation">
-                hello
-            </div>
+    public name: string = "Test";
 
-            <footer className="footer">
-              <p className="footer__text">Made by: <a className="link-primary link-text" target="_BLANK" href="//chrismakesweb.co.uk/?ref=underground-status">chrismakesweb.co.uk</a></p>
-              <p className="footer__text">Not affiliated or in any way officially connected to Transport For London (TFL)</p>
-            </footer>
-      </div>
-    );
-  }
+    constructor(props: any) {
+        super(props);
+    }
+
+    public componentDidMount() {
+        
+    }
+
+    public render() {
+        return (
+            <div id="container">
+                <div className="navigation">
+                    {this.name}
+                </div>
+
+                <footer className="footer">
+                    <p className="footer__text">Made by: <a className="link-primary link-text" target="_BLANK" href="//chrismakesweb.co.uk/?ref=underground-status">chrismakesweb.co.uk</a></p>
+                    <p className="footer__text">Not affiliated or in any way officially connected to Transport For London (TFL)</p>
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
